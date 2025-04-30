b0d81b29446847cae368b3215582a405a9acab10
Natalia Ciria Artiga <1459229@uab.cat>
2025-04-30 14:57:25 +0200

    Add and document animal_imports, prevalence_region and test_sensitivity mock datasets

---
+++ R/animal_imports.R
@@ -0,0 +1,13 @@
+#' Animal Import Data
+#'
+#' A dataset containing information about animal imports from three different regions
+#'
+#' @format ## animal_imports
+#' A data frame with 3 rows and 4 columns:
+#' \describe{
+#'   \item{origin}{Region of origin (nord, south, east)}
+#'   \item{n_farms}{Number of farms exporting animals}
+#'   \item{n_animals_mean}{Mean number of animals exported per farm}
+#'   \item{n_animals_sd}{Standard deviation of animals exported per farm}
+#' }
+"animal_imports"
---
+++ R/prevalence_region.R
@@ -0,0 +1,13 @@
+#' Regional Prevalence Data
+#'
+#' A dataset containing prevalence information for two pathogens across three regions
+#'
+#' @format ## prevalence_region
+#' A data frame with 6 rows and 4 columns:
+#' \describe{
+#'   \item{pathogen}{Pathogen identifier (a or b)}
+#'   \item{origin}{Region of origin (nord, south, east)}
+#'   \item{h_prev_min}{Minimum prevalence value}
+#'   \item{h_prev_max}{Maximum prevalence value}
+#' }
+"prevalence_region"
